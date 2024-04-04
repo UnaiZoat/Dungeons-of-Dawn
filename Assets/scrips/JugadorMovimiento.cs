ca286bb9957cb7fde321f39c7bbd95b06eaa8dcd
PabloRT010 <160068166+PabloReyesTorrejon@users.noreply.github.com>
2024-04-04 23:19:56 +0200

    Añadir assets, coleccion de monedas (falta UI)

--- Assets/scrips/JugadorMovimiento.cs
+++ Assets/scrips/JugadorMovimiento.cs
@@ -7,9 +7,14 @@ public class JugadorMovimiento : MonoBehaviour
     CharacterController characterController;
     public Camera mainCamera;
     UnityEngine.Vector3 moveInput, moveVelocity;
-    private Rigidbody rb;
     public float speed = 8;
+    public ParticleSystem particulas;
+
+    private Rigidbody rb;
     private Vector3 offset;
+    private int premios = 0;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,7 +27,7 @@ public class JugadorMovimiento : MonoBehaviour
     void Update()
     {
         Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-        mainCamera.transform.position = this.transform.position + offset;
+        mainCamera.transform.position = transform.position + offset;
         Plane groundPlane = new UnityEngine.Plane(UnityEngine.Vector3.up, UnityEngine.Vector3.zero);
         if (groundPlane.Raycast(ray, out float rayDistance)){
             UnityEngine.Vector3 point = ray.GetPoint(rayDistance);
@@ -33,5 +38,16 @@ public class JugadorMovimiento : MonoBehaviour
         moveInput = new UnityEngine.Vector3(Input.GetAxis("Horizontal"),0, Input.GetAxis("Vertical"));
         moveVelocity = moveInput.normalized * speed;
         characterController.Move(moveVelocity * speed * Time.fixedDeltaTime);
+    }
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("premio")){
+            
+            particulas.Play();
+            
+            Destroy(other.gameObject, particulas.main.duration);
+            Debug.Log("Ha tocado una estrella");
+            premios++;
+            //texto.text = "Puntuacion: " + premios;
+        }
     }
 }
