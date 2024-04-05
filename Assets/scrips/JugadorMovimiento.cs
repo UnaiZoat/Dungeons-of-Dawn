10aa28939f9bf9bcda6a9ac033b4f0ca0f8216d5
UnaiZoat <unai.delamatavicente@alum.uca.es>
2024-04-05 12:02:28 +0200

    añadido movimiento esqueletos y ordenados elementos de las salas

--- Assets/scrips/JugadorMovimiento.cs
+++ Assets/scrips/JugadorMovimiento.cs
@@ -6,44 +6,64 @@ public class JugadorMovimiento : MonoBehaviour
 {
     CharacterController characterController;
     public Camera mainCamera;
-    UnityEngine.Vector3 moveInput, moveVelocity;
+    Vector3 moveInput, moveVelocity;
     public float speed = 8;
     public ParticleSystem particulas;
+    Animator anim;
 
-    private Rigidbody rb;
     private Vector3 offset;
     private int premios = 0;
 
-
-    // Start is called before the first frame update
     void Start()
     {
         characterController = GetComponent<CharacterController>();
-        rb = GetComponent<Rigidbody>();
         offset = mainCamera.transform.position;
+        anim = GetComponentInChildren<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-        mainCamera.transform.position = transform.position + offset;
-        Plane groundPlane = new UnityEngine.Plane(UnityEngine.Vector3.up, UnityEngine.Vector3.zero);
-        if (groundPlane.Raycast(ray, out float rayDistance)){
-            UnityEngine.Vector3 point = ray.GetPoint(rayDistance);
-            Debug.DrawLine(ray.origin, point, Color.red);
-            transform.LookAt(new UnityEngine.Vector3(point.x, transform.position.y,point.z));
+        // Detecta el movimiento del jugador
+        moveInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        moveVelocity = moveInput.normalized * speed;
+
+        // Si el jugador se está moviendo, activa la animación de caminar
+        if (moveInput.magnitude > 0)
+        {
+            anim.SetFloat("movimientos", 1.0f);
+        }
+        else
+        {
+            // Si el jugador no se está moviendo, activa la animación de idle
+            anim.SetFloat("movimientos", 0.0f);
         }
 
-        moveInput = new UnityEngine.Vector3(Input.GetAxis("Horizontal"),0, Input.GetAxis("Vertical"));
-        moveVelocity = moveInput.normalized * speed;
-        characterController.Move(moveVelocity * speed * Time.fixedDeltaTime);
+        // Aplica movimiento al CharacterController
+        characterController.Move(moveVelocity * speed * Time.deltaTime);
+
+        // Rota el personaje para que mire hacia la dirección del movimiento
+        if (moveInput.magnitude > 0)
+        {
+            transform.LookAt(transform.position + moveInput.normalized);
+        }
+
+        // Actualiza la posición de la cámara
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        mainCamera.transform.position = transform.position + offset;
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        if (groundPlane.Raycast(ray, out float rayDistance))
+        {
+            Vector3 point = ray.GetPoint(rayDistance);
+            Debug.DrawLine(ray.origin, point, Color.red);
+            transform.LookAt(new Vector3(point.x, transform.position.y, point.z));
+        }
     }
-    private void OnTriggerEnter(Collider other) {
-        if (other.gameObject.CompareTag("premio")){
-            
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("premio"))
+        {
             particulas.Play();
-            
             Destroy(other.gameObject, particulas.main.duration);
             Debug.Log("Ha tocado una estrella");
             premios++;
--- Assets/scrips/enemigo.cs
+++ Assets/scrips/enemigo.cs
@@ -2,25 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class enemigo : MonoBehaviour
+public class Enemigo : MonoBehaviour
 {
-    UnityEngine.AI.NavMeshAgent pathfinder;
-    Transform target;
+    [SerializeField] List<Transform> wayPoints;
+    public float moveSpeed = 10f;
+    public float distanciaCambio = 2f;
+    byte siguientePosicion = 0;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        pathfinder = GetComponent<UnityEngine.AI.NavMeshAgent>();
-        target = GameObject.FindGameObjectWithTag("Jugador").transform;
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        pathfinder.SetDestination(target.position);
+        transform.position = Vector3.MoveTowards(transform.position, wayPoints[siguientePosicion].transform.position, moveSpeed * Time.deltaTime);
+        if (Vector3.Distance(transform.position, wayPoints[siguientePosicion].transform.position) < distanciaCambio) {
+            siguientePosicion++;
+            if (siguientePosicion >= wayPoints.Count){ 
+                siguientePosicion = 0;
+            }
+        }
 
+        // Aplicar rotación cuando cambia de waypoint o vuelve al inicio
+        if (siguientePosicion < wayPoints.Count) {
+            Vector3 direction = (wayPoints[siguientePosicion].transform.position - transform.position).normalized;
+            Quaternion lookRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+        } else {
+            // Rotar hacia atrás al primer waypoint
+            Vector3 direction = (wayPoints[0].transform.position - transform.position).normalized;
+            Quaternion lookRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+        }
     }
-
 }
