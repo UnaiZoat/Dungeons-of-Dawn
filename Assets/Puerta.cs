db55e76969de6fe533880299f869d61a5896c477
PabloRT010 <160068166+PabloReyesTorrejon@users.noreply.github.com>
2024-04-10 14:14:36 +0200

    Abrir puerta no funciona

---
+++ Assets/Puerta.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class Puerta : MonoBehaviour
+{
+    public JugadorMovimiento jugadorMovimiento;
+    public int llavesNecesarias = 1;
+
+    void Start()
+    {
+        // Suscríbete al evento
+        JugadorMovimiento.onLlaveRecogida += LlaveRecogida;
+    }
+
+
+    void LlaveRecogida(string tipoLlave)
+    {
+         if (tipoLlave == "normal" && jugadorMovimiento.llavesNormales > 0)
+        {
+            jugadorMovimiento.llavesNormales++;
+            jugadorMovimiento.textoLlavesNormales.text = "X" + jugadorMovimiento.llavesNormales;
+            
+        }
+        else if (tipoLlave == "dorada" && jugadorMovimiento.llavesDoradas > 0)
+        {
+            jugadorMovimiento.llavesDoradas++;
+            jugadorMovimiento.textoLlavesDoradas.text = "X" + jugadorMovimiento.llavesDoradas;
+            
+        }
+    }
+    /*
+    void AbrirPuerta()
+    {
+        Destroy(gameObject);
+    }*/
+    private void OnCollisionEnter(Collision other) {
+        if (other.gameObject.CompareTag("Jugador"))
+        {
+            if (jugadorMovimiento.llavesNormales >= llavesNecesarias)
+            {
+                jugadorMovimiento.llavesNormales -= llavesNecesarias;
+                jugadorMovimiento.textoLlavesNormales.text = "X" + jugadorMovimiento.llavesNormales;
+                Destroy(gameObject);
+            }
+        }   
+    }
+}
